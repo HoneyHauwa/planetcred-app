c6c964e9ce70acaa7d3ce616e051f11b8d405b85
honey hauwa <hauwa1602@gmail.com>
2026-07-03 00:34:12 +0100

    Add Google Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ReactGA from "react-ga4";
 import App from "./App";
 import "./index.css";
+
+ReactGA.initialize("G-BNWYZMF70W");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
